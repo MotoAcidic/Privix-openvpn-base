be524fe39e287ac63450ef1c30bd138cdfbce20f
TFinch <travisfinch01@gmail.com>
2019-07-23 22:24:06 -0400

    Fix echo's

--- Install.sh
+++ Install.sh
@@ -185,52 +185,51 @@ verb 4
 log-append /var/log/openvpn.log
 EOF
 
-echo Let’s copy across the easy-rsa generation files
+echo "Let’s copy across the easy-rsa generation files"
 cp -r /usr/share/easy-rsa/ /etc/openvpn/
 
-echo You can edit the variables in /etc/openvpn/easy-rsa/vars but this is not required and since we are wanting to set this up as quickly as possible we will skip this
-openssl dhparam -out /etc/openvpn/dh2048.pem 2048
+echo "You can edit the variables in /etc/openvpn/easy-rsa/vars but this is not required and since we are wanting to set this up as quickly as possible we will skip this
+openssl dhparam -out /etc/openvpn/dh2048.pem 2048"
 
-echo This will take some time and will output numerous dots and + signs.
+echo "This will take some time and will output numerous dots and + signs."
 ./clean-all && ./build-ca
 
-echo You will be asked to enter a bunch of variables, you can just keep pressing enter and use the default values
+echo "You will be asked to enter a bunch of variables, you can just keep pressing enter and use the default values"
 
-echo Next let’s generate the server.key file
+echo "Next let’s generate the server.key file"
 ./build-key-server server
 
-echo Just like above, you can keep pressing ENTER and use the default variables the only additional thing it will bring up is the certification request, you can use the default values for this too.
+echo "Just like above, you can keep pressing ENTER and use the default variables the only additional thing it will bring up is the certification request, you can use the default values for this too."
 
 sleep 5s
 
-echo When it asks you if you want to Sign the certificate? [y/n] Choose yes (enter y)
+echo "When it asks you if you want to Sign the certificate? [y/n] Choose yes (enter y)"
 
 sleep 5s
 
-echo Choose yes (enter y) 1 out of 1 certificate requests certified, commit? [y/n] Choose yes (enter y)
+echo "Choose yes (enter y) 1 out of 1 certificate requests certified, commit? [y/n] Choose yes (enter y)"
 
 #Setting up iptables
-echo You will need to know what your interface name is which you can get from ifconfig or alternatively use this command:
+echo "You will need to know what your interface name is which you can get from ifconfig or alternatively use this command:"
 iptables -t nat -A POSTROUTING -o `ip route get 8.8.8.8 | awk '{ print $5; exit }'` -j MASQUERADE
 
-echo Now we have to enable IP forwarding by executing the following command:
+echo "Now we have to enable IP forwarding by executing the following command:"
 sudo sysctl -w net.ipv4.ip_forward=1
 
 #Add User Now
-echo Adding User
+echo "Adding User"
 useradd vpnusername
 
-echo Now set a password:
+echo "Now set a password:"
 passwd vpnusername
 
-echo Restarting VPN
+echo "Restarting VPN"
 service openvpn restart
 
-echo VPN install complete. 
+echo "VPN install complete. "
 
 
             ;;
 			3)
-			echo Full list of commands: root@vpn-tutorial:/etc/openvpn# service openvpn Usage: /etc/init.d/openvpn {start|stop|reload|restart|force-reload|cond-restart|soft-restart|
-
+			echo "Full list of commands: root@vpn-tutorial:/etc/openvpn# service openvpn Usage: /etc/init.d/openvpn {start|stop|reload|restart|force-reload|cond-restart|soft-restart|"
 esac
