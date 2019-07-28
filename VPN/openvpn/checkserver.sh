f2eb532f04fd337597b03e50e67e0c6af494bd6f
TFinch <travisfinch01@gmail.com>
2019-07-28 12:59:23 -0400

    Remove unwanted files

--- VPN/openvpn/checkserver.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-RET=$(pgrep openvpn)
-
-if [ $? -eq 1 ]; then
-	systemctl restart openvpn@openvpn-server
-fi
--- VPN/pptp/checkserver.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-RET=$(pgrep pptpd)
-
-if [ $? -eq 1 ]; then
-	systemctl restart pptpd
-fi
